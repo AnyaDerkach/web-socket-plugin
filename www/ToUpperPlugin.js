4cdcd76b535b3e7c159aade81a332f8093852ec7
Anna Derkach <aderkach@plexteq.com>
2016-04-28 17:59:52 +0300

    no message

---
+++ www/ToUpperPlugin.js
@@ -0,0 +1,8 @@
+﻿// JavaScript
+var ToUpperPlugin = {
+    ToUpper: function (successCallback, errorCallback, strInput) {
+        cordova.exec(successCallback, errorCallback, "ToUpperPlugin", "ToUpper", [strInput]);
+    }
+}
+
+module.exports = ToUpperPlugin;
